59b168474fa6550b46a4cf16ec4d8c68ebff2e92
Luke Fender <lfender6445@gmail.com>
2018-04-26 12:38:18 -0400

    Updatas

--- server/server.py
+++ server/server.py
@@ -1,46 +1,53 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
 import logging
 import json
+from os import curdir, sep, chdir
 import subprocess
-import os
+import time
 
 class S(BaseHTTPRequestHandler):
-
-    # support curl -I
-    def do_HEAD(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/html")
-        s.end_headers()
-
-    def _set_response(self):
+    def _set_response(self, type):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', type)
         self.end_headers()
 
     def do_GET(self):
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        if self.path == "/foo":
-            print('bar')
-        if self.path == "/run_test":
-            logging.info('Running taurus...\n')
-            self.wfile.write("In {}".format(self.path).encode('utf-8'))
-            subprocess.run("bzt taurus.yml", shell=True)
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        if self.path.startswith("/run_test"):
+            self.run_test()
+        else:
+            self.return_log()
+        return
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+    def return_log(self):
+        try:
+            f = open(curdir + sep + self.path, 'rb')
+            if self.path.endswith('csv'):
+                self._set_response('application/csv')
+            else:
+                self._set_response('application/xml')
+            self.wfile.write(f.read())
+            f.close()
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+    def run_test(self):
+        self._set_response('application/json')
+        timestamp = {'xml_route': '/source/server/logs/' + str(time.time()) + '.xml', 'csv_route': 'log/' + str(time.time()) + '.csv'}
+        json_timestamp = json.dumps(timestamp)
+        self.wfile.write(json_timestamp.encode('utf-8'))
+        subprocess.run("bzt taurus.yml", shell=True)
+        self.create_timestamped_logs(timestamp)
+
+    def create_timestamped_logs(self, timestamp):
+        subprocess.run("touch {}".format(timestamp['xml_route']), shell=True)
+        # subprocess.run("touch {}".format(timestamp['csv_route']), shell=True)
+        subprocess.run("mv /source/server/results.xml {}".format(timestamp['xml_route']), shell=True)
+        # subprocess.run("mv /source/server/results.csv {}".format(timestamp['csv_route']), shell=True)
 
 def run(server_class=HTTPServer, handler_class=S, port=8000):
     path='/source/server'
-    os.chdir(path)
+    chdir(path)
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
@@ -53,9 +60,6 @@ def run(server_class=HTTPServer, handler_class=S, port=8000):
     logging.info('Stopping httpd...\n')
 
 if __name__ == '__main__':
-    path="/source/server/"
-    os.chdir(path)
-
     from sys import argv
 
     if len(argv) == 2:
